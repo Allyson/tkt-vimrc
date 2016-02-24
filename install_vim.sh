a6909399804e35d0e91aa980b3bf7f11ddf82d43
Takaite Takehara <takaite@brasilafora.com.br>
2016-02-24 12:36:23 -0300

    small fix

--- install_vim.sh
+++ install_vim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PWD`pwd`
+PWD=`pwd`
 
 if [ ! -e generate.vim ]; then
 	echo "Missing generate.vim. Please clone using this command"
@@ -10,5 +10,18 @@ fi
 if [ -e ~/.vimrc ]; then
 	echo "I found a .vimrc to this user. If you choose continue"
 	echo "all configuration will be deleted. What do you want?"
-	echo "(C)ontinue or (A)bort?"
-	
+
+	read -p "(C)ontinue or (A)bort? " -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ ! $REPLY =~ ^[Cc]$ ]]
+	then
+	    exit
+	else
+	    rm -rf ~/.vim*
+	fi
+fi
+
+rm -rf ~/.vim*
+cp ./generate.vim ~/.vimrc
+cp ./.vimrc.local ~/
+vim +NeoBundleInstall +qall
