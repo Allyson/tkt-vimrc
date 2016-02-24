88911cccbe79ae5d945cb188eee1d6a480b760f6
Takaite Takehara <takaite@brasilafora.com.br>
2016-02-24 00:39:10 -0300

    Missing 'v' at the end of virtualenv

--- vm_init.sh
+++ vm_init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt-get install -y build-essential python-virtualen git cmake python-dev
+apt-get install -y build-essential python-virtualenv cmake python-dev
 curl -sL https://deb.nodesource.com/setup | bash -
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 sudo apt-get update
 
-apt-get install nodejs
+apt-get install -y nodejs
 apt-get install -y mongodb-org
 
 
