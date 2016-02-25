8e86bcf5248c8f292f380c80a19285af354de04e
Takaite Takehara <takaite@brasilafora.com.br>
2016-02-25 12:06:53 -0300

    Implemented LC_ALL configuration and ssh port change

--- vm_init.sh
+++ vm_init.sh
@@ -8,8 +8,27 @@ sudo apt-get update
 apt-get install -y build-essential cmake python-virtualenv cmake python-dev
 apt-get install -y nodejs
 apt-get install -y mongodb-org
+apt-get install -y ntp
 
+if [ ! -e /swapfile ]
+then
+	fallocate -l 4G /swapfile
+	chmod 600 /swapfile
+	mkswap /swapfile
+	swapon /swapfile
+	echo "/swapfile none swap sw 0 0" >> /etc/fstab
+fi
+
+#	Changes ssh port to 2324
+sed -i.bak 's/\(Port\s*\)\([0-9]\+\)/# \1\2\n\12324/gm' /etc/ssh/sshd_config
 
 #	This should be done to fix missing locale on mongodb
 #
-echo 'LC_ALL="en_US.UTF-8"' >> /etc/environment
+LC_COUNT=`grep '^LC_ALL' /etc/environment | wc -l`
+
+if [ ${LC_COUNT} -eq 0 ] && [ ${LC_ALL:-'0'} == '0' ]
+then
+	echo 'LC_ALL="en_US.UTF-8"' >> /etc/environment
+else
+	echo '***** LC_ALL already configured *****'
+fi
